ab7163c59cf2628af36a6e4e2630aabb009b4d27
conorcussell <conor@thetab.com>
2017-02-20 11:07:01 +0000

    Adding atomic block diff stuff

---
+++ test/contentState.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "entityMap": {},
+  "blocks": [
+    {
+      "key":"8ai0j",
+      "text":"This is the content before image",
+      "type":"unstyled",
+      "depth":0,
+      "inlineStyleRanges":[],
+      "entityRanges":[],
+      "data": {}
+    }
+  ]
+}
---
+++ test/diffAtomicBlocks.spec.js
@@ -0,0 +1,36 @@
+import { expect } from 'chai';
+var contentStateBefore = require('./contentstate');
+var contentStateAfter = require('./contentstatewithimage');
+import { diffAtomicBlocks } from '../lib/diffAtomicBlocks';
+
+describe('diffAtomicBlocks', () => {
+  context('with no difference', () => {
+    it('returns false', () => {
+      const actual = diffAtomicBlocks(contentStateBefore, contentStateBefore);
+      expect(actual).to.eq(false);
+    });
+  });
+
+  context('with differences', () => {
+    it('returns an array representing the differences', () => {
+      const actual = diffAtomicBlocks(contentStateBefore, contentStateAfter);
+      expect(Array.isArray(actual)).to.eq(true);
+    });
+
+    context('with an atomic block added', () => {
+      it('shows the atomic blocks which were added', () => {
+        const actual = diffAtomicBlocks(contentStateBefore, contentStateAfter);
+        const expected = [ [1, '86ekh'] ];
+        expect(actual).to.deep.eq(expected);
+      });
+    });
+
+    context('with an atomic block removed', () => {
+      it('shows the atomic blocks which were removed', () => {
+        const actual = diffAtomicBlocks(contentStateAfter, contentStateBefore);
+        const expected = [ [-1, '86ekh'] ];
+        expect(actual).to.deep.eq(expected);
+      });
+    });
+  });
+});
